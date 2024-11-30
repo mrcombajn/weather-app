bba2152a7d20d98f346eb80c91d7d6917f30edb1
Mateusz Ostrowski <mateusz.ostro@o2.pl>
2024-11-30 23:28:31 +0100

    cleared app data

--- src/App.js
+++ src/App.js
@@ -34,25 +34,4 @@ function contactApi() {
     );
 }
 
-class WeatherInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {actualCityWeather: contactApi()}
-        console.log(this.state.actualCityWeather.value)
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <header></header>
-                <main>
-                    <p>Aktualna temperatura:</p>
-                    <p>Minimalna odczuwalna temperatura:</p>
-                    <p>Maksymalna odczuwalna temperatura:</p>
-                </main>
-
-            </div>
-        );
-    }
-}
 export default App;
--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -3,13 +3,23 @@ import WeatherTile from "./WeatherTile";
 
 
 class WeatherInfo extends Component {
-
     constructor(props) {
         super(props);
+        //this.state = {actualCityWeather: contactApi()}
+        //console.log(this.state.actualCityWeather.value)
     }
+
     render() {
         return (
-            console.log(this.props.state)
+            <div className="container">
+                <header></header>
+                <main>
+                    <p>Aktualna temperatura:</p>
+                    <p>Minimalna odczuwalna temperatura:</p>
+                    <p>Maksymalna odczuwalna temperatura:</p>
+                </main>
+
+            </div>
         );
     }
 }
